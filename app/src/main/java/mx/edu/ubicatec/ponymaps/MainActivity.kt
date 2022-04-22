83a7ae6d3116f4b9f27376068b169210040f575f
Humberto Maldonado <lemon_tea@outlook.es>
2022-04-22 01:37:33 -0500

    Fixed Fragments not Working

--- app/src/main/java/mx/edu/ubicatec/ponymaps/MainActivity.kt
+++ app/src/main/java/mx/edu/ubicatec/ponymaps/MainActivity.kt
@@ -3,6 +3,7 @@ package mx.edu.ubicatec.ponymaps
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -11,13 +12,16 @@ import mx.edu.ubicatec.ponymaps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+    /*
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root)*/
+
+        /*
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
 
@@ -25,16 +29,17 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment?.findNavController()
         if (navController != null) {
             bottomNavigationView.setupWithNavController(navController)
-        }
+        } */
 
-        /*
-        val navView: BottomNavigationView = binding.navView
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         navView.setupWithNavController(navController)
-
+        /*
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)*/
