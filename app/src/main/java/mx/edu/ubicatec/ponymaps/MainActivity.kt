9f86700f221e192fbbfe54a363a2bf4ebdb55562
Humberto Maldonado <lemon_tea@outlook.es>
2022-05-07 16:42:42 -0500

    Map Fragment

--- app/src/main/java/mx/edu/ubicatec/ponymaps/MainActivity.kt
+++ app/src/main/java/mx/edu/ubicatec/ponymaps/MainActivity.kt
@@ -8,43 +8,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.maps.CameraUpdateFactory
 import mx.edu.ubicatec.ponymaps.databinding.ActivityMainBinding
 
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.MarkerOptions
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    //private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    /*
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)*/
-
-        /*
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
-        val navController = navHostFragment?.findNavController()
-        if (navController != null) {
-            bottomNavigationView.setupWithNavController(navController)
-        } */
-
-        setContentView(R.layout.activity_main)
-
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        setContentView(binding.root)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -52,15 +27,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         navController = navHostFragment.navController
 
         navView.setupWithNavController(navController)
-        /*
-        val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)*/
     }
 
-
-
-    override fun onMapReady(googleMap: GoogleMap) {
+    /* override fun onMapReady(googleMap: GoogleMap) {
 
         googleMap.setMinZoomPreference(DEFAULT_MIN_ZOOM);
         googleMap.setMaxZoomPreference(DEFAULT_MAX_ZOOM);
@@ -80,7 +49,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         private val ITM_CAMERA = CameraPosition.Builder()
             .target(LatLng(19.722037, -101.184835)).zoom(15.0f).bearing(0f).tilt(0f).build()
 
-    }
-
+    } */
 
 }
--- app/src/main/java/mx/edu/ubicatec/ponymaps/ui/mapa/MapaFragment.kt
+++ app/src/main/java/mx/edu/ubicatec/ponymaps/ui/mapa/MapaFragment.kt
@@ -7,12 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.*
 
-
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
@@ -22,93 +18,23 @@ import mx.edu.ubicatec.ponymaps.MainActivity
 import mx.edu.ubicatec.ponymaps.R
 import mx.edu.ubicatec.ponymaps.databinding.FragmentMapBinding
 
-/*class MapaFragment : Fragment(), OnMapReadyCallback {
-
-    companion object {
-        private const val MAP_VIEW_BUNDLE_KEY = "mapview_bundle_key"
-    }
-
-    private var _binding: FragmentMapsBinding? = null
-    private val binding: FragmentMapsBinding get() = _binding!!
-
-    private var hasMapConfigured: Boolean = false
-    // Used to persist map state during fragment transaction.
-    private lateinit var persistedMapBundle: Bundle
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        persistedMapBundle = savedInstanceState?.getBundle(MAP_VIEW_BUNDLE_KEY) ?: Bundle()
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMapsBinding.inflate(layoutInflater, container, false)
-        val mapViewBundle = savedInstanceState?.getBundle(MAP_VIEW_BUNDLE_KEY)
-        initMapViewState(binding.mapView, mapViewBundle)
-        binding.mapView.getMapAsync(this)
-        return binding.root
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBundle(MAP_VIEW_BUNDLE_KEY, persistedMapBundle)
-    }
-
-    // region Typical lifecycle method forwarding
-    override fun onStart() {
-        super.onStart()
-        binding.mapView.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        binding.mapView.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        binding.mapView.onStop()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        _binding?.mapView?.onLowMemory()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding.mapView.onSaveInstanceState(persistedMapBundle)
-        binding.mapView.onDestroy()
-        _binding = null
-    }
-    // endregion
-
-    private fun initMapViewState(mapView: MapView, savedMapViewBundle: Bundle?) {
-        // The state persisted across Fragment transaction.
-        if (!persistedMapBundle.isEmpty) {
-            mapView.onCreate(persistedMapBundle)
-            hasMapConfigured = true
-            return
-        }
-        // The state persisted across Fragment recreation.
-        mapView.onCreate(savedMapViewBundle)
-        hasMapConfigured = savedMapViewBundle != null
-    }
-
-    override fun onMapReady(p0: GoogleMap) {
-        TODO("Not yet implemented")
-    }
-}*/
-
 class MapaFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+
+        googleMap.setMinZoomPreference(MapaFragment.DEFAULT_MIN_ZOOM);
+        googleMap.setMaxZoomPreference(MapaFragment.DEFAULT_MAX_ZOOM);
+        googleMap.setLatLngBoundsForCameraTarget(MapaFragment.ITM);
+    }
 
     private var _binding: FragmentMapBinding? = null
 
@@ -121,49 +47,17 @@ class MapaFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
 
-
     ): View {
         val mapaViewModel = ViewModelProvider(this).get(MapaViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textViewMap
-        mapaViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-
-
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-
-
-
-}
-
-/*
-, OnMapReadyCallback
-
-val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-mapFragment.getMapAsync(this)
-
- override fun onMapReady(googleMap: GoogleMap) {
-
-        googleMap.setMinZoomPreference(DEFAULT_MIN_ZOOM);
-        googleMap.setMaxZoomPreference(DEFAULT_MAX_ZOOM);
-        googleMap.setLatLngBoundsForCameraTarget(ITM);
-
-   }
-
     companion object {
-        private val TAG = MainActivity::class.java.name
+        private val TAG = MapaFragment::class.java.name
         private const val ZOOM_DELTA = 2.0f
         private const val DEFAULT_MIN_ZOOM = 17.0f
         private const val DEFAULT_MAX_ZOOM = 22.0f
@@ -175,5 +69,4 @@ mapFragment.getMapAsync(this)
             .target(LatLng(19.722037, -101.184835)).zoom(15.0f).bearing(0f).tilt(0f).build()
 
     }
-
-    */
+}
